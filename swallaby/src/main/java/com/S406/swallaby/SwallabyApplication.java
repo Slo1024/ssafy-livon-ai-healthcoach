dafd6be02738f82fa2af20e28a7af030e7109698
DESKTOP-DLOPF19\SSAFY <happyms817@gmail.com>
2025-10-10 12:05:46 +0900

    [BE] feat: Initial project structure for backend

---
+++ swallaby/src/main/java/com/S406/swallaby/SwallabyApplication.java
@@ -0,0 +1,13 @@
+package com.S406.swallaby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwallabyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwallabyApplication.class, args);
+	}
+
+}
---
+++ swallaby/src/test/java/com/S406/swallaby/SwallabyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.S406.swallaby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwallabyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

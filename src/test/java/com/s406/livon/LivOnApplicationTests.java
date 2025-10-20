b1f5d3690f0262d012695e8fa0df2a39313382f4
jjaewooks <jj99526@naver.com>
2025-10-20 15:24:41 +0900

    feat : firstCommit

---
+++ src/test/java/com/s406/livon/LivOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.s406.livon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivOnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

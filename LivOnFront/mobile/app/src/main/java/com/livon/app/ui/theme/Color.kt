b553ff4bb549f6816e4e537260d05f0f867dc426
제효정 <jehwju1024@naver.com>
2025-10-27 08:49:46 +0900

    [front] chore: Clean Architecture 기반 패키지 구조 세팅

--- LivOnFront/mobile/app/src/main/java/com/livon/app/ui/theme/Color.kt
+++ LivOnFront/mobile/app/src/main/java/com/livon/app/ui/theme/Color.kt
@@ -2,10 +2,18 @@ package com.livon.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple80 = Color(0xFFD0BCFF)
-val PurpleGrey80 = Color(0xFFCCC2DC)
-val Pink80 = Color(0xFFEFB8C8)
+// Figma tokens
+val Basic         = Color(0xFFFFFFFF) // basic - FFFFFF
+val Main          = Color(0xFF4965F6) // main  - 4965F6
+val Sub1          = Color(0xFF77A3F3) // sub1  - 77A3F3
+val Sub2          = Color(0xFFEDF2FC) // sub2  - EDF2FC
 
-val Purple40 = Color(0xFF6650a4)
-val PurpleGrey40 = Color(0xFF625b71)
-val Pink40 = Color(0xFF7D5260)
+val Gray          = Color(0xFF878787) // gray  - 878787
+val Gray2         = Color(0xFF666666) // gray2 - 666666
+
+val Border        = Color(0xFFBABABA) // border - BABABA
+val BorderBlack   = Color(0xFF000000) // border_black - 000000
+
+val Qna           = Color(0xFFF2B1AB) // QNA - F2B1AB
+val Unclickable   = Color(0xFFE3E3E3) // unclickable - E3E3E3
+val LiveRed       = Color(0xFFFC4646) // live - FC4646

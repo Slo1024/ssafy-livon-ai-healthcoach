79435745fe141bd97a989a496ed98d525e450cbb
zeus <jj99526@naver.com>
2025-10-31 18:58:32 +0900

    [Back] feat : 채팅 조회 redis 캐싱 처리

--- LivOnBack/src/main/java/com/s406/livon/domain/goodsChat/service/GoodsChatMessageService.java
+++ LivOnBack/src/main/java/com/s406/livon/domain/goodsChat/service/GoodsChatMessageService.java
@@ -17,6 +17,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionTemplate;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
@@ -30,6 +31,8 @@ public class GoodsChatMessageService {
     private final GoodsChatRoomRepository chatRoomRepository;
     private final GoodsChatMessageRepository messageRepository;
     private final SimpMessagingTemplate messagingTemplate;
+    private final TransactionTemplate mongoTransactionTemplate;
+    private final GoodsChatCacheManager goodsChatCacheManager;
 
     private static final String MEMBER_ENTER_MESSAGE = "님이 채팅방에 입장하셨습니다.";
     private static final String MEMBER_LEAVE_MESSAGE = "님이 채팅방을 떠나셨습니다.";
@@ -38,16 +41,37 @@ public class GoodsChatMessageService {
     public void sendMessage(GoodsChatMessageRequest message) {
         User sender = findMemberById(message.getSenderId());
         GoodsChatRoom chatRoom = findByChatRoomById(message.getRoomId());
-        GoodsChatMessage chatMessage = createChatMessage(chatRoom.getId(), sender.getId(), message.getMessage(), message.getType());
-
-        // DB에 메시지 저장
-        // 채팅 데이터 저장 & 최신 채팅 내역 업데이트
-        GoodsChatMessage savedMessage = messageRepository.save(chatMessage);
-        chatRoom.updateLastChat(chatMessage.getContent(), chatMessage.getSentAt());
-
-        GoodsChatMessageResponse response = GoodsChatMessageResponse.of(savedMessage, sender);
-        sendToSubscribers(message.getRoomId(), response);
+        saveAndSendMessage(chatRoom, sender, message.getMessage(), message.getType());
     }
+
+    private void saveAndSendMessage(GoodsChatRoom chatRoom, User user, String message, MessageType type) {
+        // 채팅 도큐먼트 생성
+        GoodsChatMessage chatMessage = createChatMessage(chatRoom.getId(), user.getId(), message, type);
+
+        // 최신 채팅 내역 업데이트
+        chatRoom.updateLastChat(message, chatMessage.getSentAt());
+
+        // MongoDB 트랜잭션
+        try {
+            mongoTransactionTemplate.executeWithoutResult(status -> {
+                // 채팅 메시지 저장
+                GoodsChatMessage savedMessage = messageRepository.save(chatMessage);
+
+                // redis 캐시 저장
+                goodsChatCacheManager.storeMessageInCache(savedMessage.getChatRoomId(), savedMessage);
+
+                // 메시지 전송
+                sendToSubscribers(savedMessage.getChatRoomId(), GoodsChatMessageResponse.of(savedMessage, user));
+            });
+        } catch (Exception e) {
+            // redis 캐시 무효화
+            goodsChatCacheManager.evictMessagesFromCache(chatRoom.getId());
+
+            // JPA 트랜잭션으로 예외 전파
+            throw e;
+        }
+    }
+
 
     // 입장 및 퇴장 메시지 전송
     public void sendChatEventMessage(GoodsChatEvent event) {

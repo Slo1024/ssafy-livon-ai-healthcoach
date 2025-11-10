5fb61ed126a79ae0e8f331e69b3229d05e024aa6
정원준 <dnjswns3851@naver.com>
2025-11-10 17:51:34 +0900

    [back] fix: CORS 설정 변경, 프론트엔드 요청 경로 수정

--- LivOnBack/src/main/java/com/s406/livon/global/config/WebConfig.java
+++ LivOnBack/src/main/java/com/s406/livon/global/config/WebConfig.java
@@ -1,22 +1,22 @@
-package com.s406.livon.global.config; // 👈 본인의 config 패키지 경로로 수정
-
-import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.method.HandlerTypePredicate;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
-@Configuration
-public class WebConfig implements WebMvcConfigurer {
-    
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/api/**") // 👈 /api/v1/.. 등을 포함하도록 /api/** 설정
-                .allowedOrigins("*")   // 👈 모든 출처(file://, http://... 등)를 허용
-                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                .allowedHeaders("*")
-                .allowCredentials(false) // 👈 자격 증명(쿠키 등)을 사용하지 않는 경우
-                .maxAge(3600);
-    }
-}
+//package com.s406.livon.global.config; // 👈 본인의 config 패키지 경로로 수정
+//
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.web.bind.annotation.RestController;
+//import org.springframework.web.method.HandlerTypePredicate;
+//import org.springframework.web.servlet.config.annotation.CorsRegistry;
+//import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+//
+//@Configuration
+//public class WebConfig implements WebMvcConfigurer {
+//
+//    @Override
+//    public void addCorsMappings(CorsRegistry registry) {
+//        registry.addMapping("/api/**") // 👈 /api/v1/.. 등을 포함하도록 /api/** 설정
+//                .allowedOrigins("*")   // 👈 모든 출처(file://, http://... 등)를 허용
+//                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+//                .allowedHeaders("*")
+//                .allowCredentials(false) // 👈 자격 증명(쿠키 등)을 사용하지 않는 경우
+//                .maxAge(3600);
+//    }
+//}

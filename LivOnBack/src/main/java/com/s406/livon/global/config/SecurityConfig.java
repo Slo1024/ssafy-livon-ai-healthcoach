fbf7fb0eeef2860247ccf0bda13907ca6e3cd9ae
정원준 <dnjswns3851@naver.com>
2025-11-11 09:17:16 +0900

    [back] fix: CORS 허용 오리진 추가

--- LivOnBack/src/main/java/com/s406/livon/global/config/SecurityConfig.java
+++ LivOnBack/src/main/java/com/s406/livon/global/config/SecurityConfig.java
@@ -33,32 +33,35 @@ public class SecurityConfig {
     private final ObjectMapper objectMapper;
 
 
-//    @Bean
-//    public CorsConfigurationSource corsConfigurationSource() {
-//        CorsConfiguration config = new CorsConfiguration();
-//        // 개발용: 정확한 오리진만 명시 (credentials 쓰면 * 금지)
-//        config.setAllowedOrigins(List.of("http://localhost:3000"));
-//        config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
-//        // 프론트가 보내는 헤더들 추가 (Authorization 자주 빠짐)
-//        config.setAllowedHeaders(List.of("Content-Type","Authorization","X-Requested-With"));
-//        // 응답에서 노출할 헤더 (필요시)
-//        config.setExposedHeaders(List.of("Location"));
-//        // 쿠키/자격증명 사용 시 true (axios withCredentials=true 인 경우)
-//        config.setAllowCredentials(true);
-//        // preflight 캐시 시간(초)
-//        config.setMaxAge(Duration.ofHours(1));
-//
-//        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-//        source.registerCorsConfiguration("/**", config);
-//        return source;
-//    }
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        // 개발용: 정확한 오리진만 명시 (credentials 쓰면 * 금지)
+        config.setAllowedOrigins(List.of("http://localhost:3000"));
+        config.setAllowedOrigins(List.of("http://localhost:3001"));
+        config.setAllowedOrigins(List.of("https://k13s406.p.ssafy.io:8443"));
+        config.setAllowedOrigins(List.of("https://k13s406.p.ssafy.io"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","PATCH","DELETE","OPTIONS"));
+        // 프론트가 보내는 헤더들 추가 (Authorization 자주 빠짐)
+        config.setAllowedHeaders(List.of("Content-Type","Authorization","X-Requested-With"));
+        // 응답에서 노출할 헤더 (필요시)
+        config.setExposedHeaders(List.of("Location"));
+        // 쿠키/자격증명 사용 시 true (axios withCredentials=true 인 경우)
+        config.setAllowCredentials(true);
+        // preflight 캐시 시간(초)
+        config.setMaxAge(Duration.ofHours(1));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
+    }
 
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .csrf(csrf -> csrf.disable())
-//                .cors(Customizer.withDefaults())
+                .cors(Customizer.withDefaults())
                 .httpBasic(httpBasic -> httpBasic.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
